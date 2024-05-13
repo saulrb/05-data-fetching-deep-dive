4e509eb1fa0b4e89a4b16cdaade782d4b5980f29
saul rosales <saul.rosalesb@gmail.com>
2024-05-13 08:18:17 -0400

    Added interceptor page

---
+++ app/news/[slug]/(.)image/page.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react'
+import { notFound } from 'next/navigation'
+import { DUMMY_NEWS } from '../../../../dummy-news'
+type Props = {
+  params: any
+}
+
+const InterceptedImagePage: FC<Props> = ({ params }) => {
+  const newsSlug = params.slug
+  const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug)
+
+  if (!newsItem) {
+    notFound()
+  }
+
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  )
+}
+
+export default InterceptedImagePage
